5cbc3bc0019458f2b0a7976ee4b4db188cba5fde
Xùdōng Yáng <wyverald@gmail.com>
2022-01-21 22:23:32 +0100

    initial commit

---
+++ .github/actions/barm/index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  const token = core.getInput("token");
+  const octokit = github.getOctokit(token);
+
+  const payload = github.context.payload;
+  const command = payload.comment.body.slice(6);  // "!barm "
+
+  await octokit.issues.createComment({
+    owner: payload.repository.owner.login,
+    repo: payload.repository.name,
+    issue_number: payload.issue.number,
+    body: "hi! your command was: `" + command + "`"
+  });
+
+}
+
+run();
---
+++ README.md
@@ -0,0 +1,1 @@
+# Wyverald's Playground
