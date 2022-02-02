341786e451874d5d5674faaa6d00e2b48aa0bf19
Xùdōng Yáng <wyverald@gmail.com>
2022-02-02 16:42:00 +0100

    wow!

--- .github/actions/barm/index.js
+++ .github/actions/barm/index.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const RELEASE_BLOCKER = "potential release blocker";
+
 async function run() {
   const token = core.getInput("token");
   const octokit = github.getOctokit(token);
@@ -8,13 +10,67 @@ async function run() {
   const payload = github.context.payload;
   const command = payload.comment.body.slice(6);  // "!barm "
 
-  await octokit.rest. issues.createComment({
-    owner: payload.repository.owner.login,
-    repo: payload.repository.name,
-    issue_number: payload.issue.number,
-    body: "hi! your command was: `" + command + "`"
-  });
+  const owner = payload.repository.owner.login;
+  const repo = payload.repository.name;
+
+  if (command.startsWith("fork ")) {
+    // Check if the commenter is a collaborator of the repo
+    try {
+      await octokit.rest.repos.checkCollaborator({
+        owner,
+        repo,
+        username: payload.comment.user.login,
+      });
+    } catch (err) {
+      throw `user ${payload.comment.user.login} has no collaborator access in repo ${owner}/${repo}`;
+    }
+
+    // Find the milestone corresponding to this branch
+    const branch = command.slice(5);
+    var milestoneNumber = null;
+    for await (const milestone of octokit.paginate.iterator(
+      octokit.rest.issues.listMilestones,
+      {owner, repo}
+    )) {
+      if (milestone.title === `${branch} release blockers`) {
+        milestoneNumber = milestone.number;
+        break;
+      }
+    }
+    if (milestoneNumber === null) {
+      throw `no release blocker milestone found for ${branch}`;
+    }
+
+    // Create an issue on this milestone
+    await octokit.rest.issues.create({
+      owner,
+      repo,
+      title: `[${branch}] ` + payload.issue.title,
+      body: `Forked from #${payload.issue.number}`,
+      milestone: milestoneNumber,
+    });
+
+    // Remove the "potential release blocker" label if it's present
+    if (payload.issue.labels.some(label => label.name === FLAGGED_LABEL)) {
+      await octokit.rest.issues.removeLabel({
+        owner,
+        repo,
+        issue_number: payload.issue.number,
+        name: FLAGGED_LABEL,
+      });
+    }
+  } else if (command === "flag") {
+    await octokit.rest.issues.addLabel({
+      owner,
+      repo,
+      issue_number: payload.issue.number,
+      name: FLAGGED_LABEL,
+    });
+  }
 
 }
 
-run();
+run().then(
+  result => { core.info(result); },
+  err => { core.setFailed(err); }
+);
